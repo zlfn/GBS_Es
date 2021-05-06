eff7514f6caa4353a9456b22bfe688f4b2e3d393
zlfn <elusive1102@naver.com>
2021-05-06 22:56:49 +0900

    Added backbutton to lgsActivity

--- app/src/main/java/com/dayo/executer/lgs_MainActivity.kt
+++ app/src/main/java/com/dayo/executer/lgs_MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.util.Log
 import android.view.View
 import android.widget.TextView
@@ -18,10 +19,15 @@ import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 
 
+
 class lgs_MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.lgs_activity_main)
+        val backButton: Button = findViewById(R.id.button)
+        backButton.setOnClickListener{
+            finish()
+        }
 
         var vifo = ""
         CoroutineScope(Dispatchers.Default).launch {
@@ -108,5 +114,6 @@ class lgs_MainActivity : AppCompatActivity() {
                         .create().show()
             }
         }
+
     }
 }
