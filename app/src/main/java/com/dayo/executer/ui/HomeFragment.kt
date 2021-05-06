7fdd80e2be11e057145d00f000b7693aec6ca4eb
dy <dayo05@daum.net>
2021-05-07 01:58:46 +0900

    Make onCreate call once

--- app/src/main/java/com/dayo/executer/ui/HomeFragment.kt
+++ app/src/main/java/com/dayo/executer/ui/HomeFragment.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.*
 import com.dayo.executer.AsckActivity
 import com.dayo.executer.MainActivity
