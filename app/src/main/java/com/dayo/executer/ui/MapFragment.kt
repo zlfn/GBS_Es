c278c35c33c815400bdf8bc4228de2a78e3d4c0f
zlfn <elusive1102@naver.com>
2021-05-10 20:42:31 +0900

    Added ChangeIcon Feature 한시간의 뻘짓끝에 성공함 코드가 더러운건 나중에 청소함ㅇㅇ

--- app/src/main/java/com/dayo/executer/ui/MapFragment.kt
+++ app/src/main/java/com/dayo/executer/ui/MapFragment.kt
@@ -1,13 +1,24 @@
 package com.dayo.executer.ui
 
+import android.content.ClipData
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.graphics.drawable.Icon
+import android.telephony.ims.ImsMmTelManager
+import android.view.*
+import android.widget.Toolbar
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.dayo.executer.R
+import com.dayo.executer.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MapFragment : Fragment() {
+    lateinit var m : MainActivity
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?): View? {
@@ -16,7 +27,22 @@ class MapFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+        m = (activity as MainActivity)
+        var navView: BottomNavigationView = m.findViewById(R.id.nav_view)
+        var menunav: Menu = navView.getMenu()
+        var mapitem: MenuItem = menunav.findItem(R.id.navigation_map)
+
+        mapitem.setIcon(R.drawable.ic_baseline_search_24)
+        //TODO: AndroidSlidingUpPanel을 이용해서 검색버튼 누르면 검색창 띄우기
     }
 
+    override fun onStop() {
+        super.onStop()
+        m = (activity as MainActivity)
+        var navView: BottomNavigationView = m.findViewById(R.id.nav_view)
+        var menunav: Menu = navView.getMenu()
+        var mapitem: MenuItem = menunav.findItem(R.id.navigation_map)
 
+        mapitem.setIcon(R.drawable.ic_baseline_map_24)
+    }
 }
